d1a7e721577a98417bfd5cfb15ff76dec216e960
rhengeveldbordex <113895083+rhengeveldbordex@users.noreply.github.com>
2024-06-06 09:51:37 +0200

    Added GitHub action

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="transsmart_connector",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'requests >= 2.31.0',
+        'python-dotenv >= 1.0.1'
+    ],
+    author="Robert Hengeveld",
+    author_email="rhengeveld@bordex.nl",
+    description="Transsmart Connector is a Python package to connect to the Transsmart API. It provides a simple interface to interact with the Transsmart API. The package is built on top of the requests library. The package is designed to be used in a business environment to automate the process of creating shipments, labels, and documents.",
+    url="https://github.com/rhengeveldbordex/transsmart-connector",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+)
--- transsmart_connector/test.py
+++ transsmart_connector/test.py
@@ -5,6 +5,13 @@ from transsmart_connector import TranssmartConnector
 
 load_dotenv()
 
+# This is a test script to test the TranssmartConnector class
+# Make sure to set the environment variables in a .env file in the root of this project
+# The following environment variables are required:
+# TRANSSMART_ACCOUNT
+# TRANSSMART_USERNAME
+# TRANSSMART_PASSWORD
+
 # Initialize the TranssmartConnector class
 transsmart = TranssmartConnector()
 
@@ -12,7 +19,23 @@ transsmart = TranssmartConnector()
 shipments = transsmart.get_shipments()
 print(shipments)
 
-#GET shipment by ID
-shipment_id = 'PNL-1717597661'
+# GET shipment by ID
+shipment_id = 'YOUR_SHIPMENT_ID_HERE'
 shipment = transsmart.get_shipment(shipment_id)
 print(shipment)
+
+# Cancel shipment by ID
+shipment_id = 'YOUR_SHIPMENT_ID_HERE'
+response = transsmart.cancel_shipment(shipment_id)
+print(response)
+
+# GET labels by shipment ID
+shipment_id = 'YOUR_SHIPMENT_ID_HERE'
+labels = transsmart.get_labels(shipment_id)
+print(labels)
+
+# GET documents by shipment ID
+shipment_id = 'YOUR_SHIPMENT_ID_HERE'
+documents = transsmart.get_documents(shipment_id)
+print(documents)
+
